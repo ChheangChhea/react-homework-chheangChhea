9db18eefb64a20cf08904b4bbaebdd72a1717a51
Chheang.chhea <chheangchhea008@gmail.com>
2023-04-18 13:28:18 +0700

    react-homework-chheang chhea

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import Nav from './componemt/Nav';
+import Contain from './componemt/Contain';
+import Feedback from './componemt/Feedback';
+import Footer from './componemt/Footer';
+function App() {
+  return (
+    <div >
+     {/* <h1>Hello </h1> */}
+     <Nav/>
+     <Contain/>
+     <Feedback/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
